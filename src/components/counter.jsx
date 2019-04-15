bd6cde9a98663a8f3db89921f280ca5c6c0eb9e4
StephenPopovich <stephenpopovich16@gmail.com>
2019-04-14 21:19:03 -0700

    add style class

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+ state = {
+     count: 0,
+
+ };
+ styles = {
+     fontSize: '50px',
+     fontWeight: "bold"
+ }
     render() { 
-        return <h1>Hello World</h1>;
-    }
+        
+        return (
+         <div>
+
+            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-lg">Increment</button>
+          </div>
+     );
+  }
+
+  formatCount(){
+      const {count} = this.state;
+      const x = <h1>Zero</h1>;
+      return count === 0 ?  x : count;
+  }
 }
  
 export default Counter;
